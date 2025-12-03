ad15173da7f299134e8c286f8d28ea6e1f1fa956
Aesteric <aviral_vaidya@brown.edu>
2025-12-02 19:41:43 -0500

    adding placeholder file for naieve attention hip implementation

---
+++ src/naieve_attention_implementation.cpp
@@ -0,0 +1,14 @@
+/*
+Non-flashattention implementation for HIP.
+
+This is regular attention implementation that does not use any flashattention optimizations.
+*/
+
+#include <hip/hip_runtime.h>
+#include <cmath>
+
+int main() {
+
+
+    
+}
